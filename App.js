2112d8778b334474bc4fba55573b4d259531dda6
Anca Puscasu <ancapuscasu@ymail.com>
2022-03-02 13:01:40 -0500

    Added navigation to App

--- App.js
+++ App.js
@@ -1,15 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+// importing screens we want to navigate between
+import HomeScreen from './components/HomeScreen';
+import Chat from './components/Chat';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import 'react-native-gesture-handler';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react';
+
+// create navigator
+const Stack = createStackNavigator();
+
+export default class App extends React.Component {
+
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator
+        initialRouteName= "Home Screen">
+          <Stack.Screen
+            name="Home Screen"
+            component={HomeScreen} />
+          <Stack.Screen
+            name="Chat"
+            component={Chat} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
 
+// style sheet at the bottom
 const styles = StyleSheet.create({
   container: {
     flex: 1,
