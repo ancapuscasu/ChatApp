6520c04d5b9ca7f07303c8a0b061326c7365aba3
Anca Puscasu <ancapuscasu@ymail.com>
2022-03-03 13:45:48 -0500

    Added Chat UI with some default messages and custom styling

--- components/Chat.js
+++ components/Chat.js
@@ -1,32 +1,96 @@
-import React, { useEffect } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useEffect, useState, useCallback } from "react";
+
+//importing Gifted Chat (UI for chat) component
+import { GiftedChat, Bubble } from "react-native-gifted-chat";
+
+//importing UI components from react-native
+import { View, StyleSheet, KeyboardAvoidingView, Platform } from "react-native";
+
 
 
 // The application’s Chat component that renders the chat UI
-const Chat = ({route, navigation}) => {
-  console.log(route);
+export default function Chat ({route, navigation}) {
+  const [ messages, setMessages ] = useState([]);
 
-  let name = route.params.name;
-  const { bgColor } = route.params;
+  //props from HomeScreen
+  const name = route.params.name;
+  const bgColor = route.params.bgColor;
+  console.log(bgColor);
 
+  //Setting user name as the header title
   useEffect(() => {
     navigation.setOptions({ title:name });
   }, []);
-  
 
-  const styles = StyleSheet.create({
-    chatScreen: {
-      flex: 1,
-      backgroundColor: bgColor
-    },
-  });
+
+  //Setting default messages; one from a default user and one from the system announcing who just entered the chat
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'Hello developer',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+      {
+        _id: 2,
+        text: `${name} has entered the chat`,
+        createdAt: new Date(),
+        system: true,
+       },
+    ])
+  }, [])
+
+  //function to add message to messages[array]; takes array of existing messages and appends current message to it
+  const onSend = useCallback((messages = []) => {
+    setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
+  }, [])
+
+
+  //function to adjust Bubble backgroundColor to color chosen by user
+  const renderBubble = (props) => {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: bgColor,
+            padding: 2,
+          },
+          left: {
+            backgroundColor: "#E2E2E2",
+            padding: 2
+          }
+        }}
+      />
+    )
+  }
+  
 
   return (
     <View style={styles.chatScreen}>
+      <GiftedChat
+      renderBubble={renderBubble}
+      messages={messages}
+      onSend={messages => onSend(messages)}
+      user={{
+        _id: 1,
+      }}
+    />
+    { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null}
     </View>
   )
 }
 
 
-export default Chat;
+//styles 
+const styles = StyleSheet.create({
+  chatScreen: {
+    flex: 1,
+  },
+});
 
