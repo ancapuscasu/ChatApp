e694ff70e15386809a2dd4dad40870955da69ab9
Anca Puscasu <ancapuscasu@ymail.com>
2022-03-11 00:01:34 -0500

    cleaned up code

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
+import React from 'react';
 import { StyleSheet } from 'react-native';
-// importing screens we want to navigate between
 import HomeScreen from './components/HomeScreen';
 import Chat from './components/Chat';
 
@@ -7,7 +7,6 @@ import 'react-native-gesture-handler';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import React from 'react';
 
 // create navigator
 const Stack = createStackNavigator();
@@ -17,14 +16,9 @@ export default class App extends React.Component {
   render() {
     return (
       <NavigationContainer>
-        <Stack.Navigator
-        initialRouteName= "Home Screen">
-          <Stack.Screen
-            name="Home Screen"
-            component={HomeScreen} />
-          <Stack.Screen
-            name="Chat"
-            component={Chat} />
+        <Stack.Navigator initialRouteName = "Home Screen">
+          <Stack.Screen name="Home Screen" component={HomeScreen} />
+          <Stack.Screen name="Chat" component={Chat} />
         </Stack.Navigator>
       </NavigationContainer>
     );
